085a94c21f4da0cd52164a316344e1c4a8cd4d6a
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-04-12 19:46:08 -0500

    mouseover, mouseleave and click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+
+//mouseover
+let mouseOver = document.querySelector("img");
+mouseOver.addEventListener("mouseover", (event) => {
+    event.target.style.transform = "scale(1.2)"
+} )
+
+//mouseleave
+let mouseLeave = document.querySelector("img");
+mouseLeave.addEventListener("mouseleave", (event) => {
+    event.target.style.transform = "scale(1.0)"
+})
+
+//click
+let button = document.querySelectorAll('.btn');
+for (let b = 0; b < button.length; b++) {
+    let btn = button[b];
+    btn.addEventListener('click', function() {
+     alert("Thank you for signing up");
+     event.stopPropagation();
+    })
+};
