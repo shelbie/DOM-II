0412093f2afd1bb18c7c7c4237f9789d901f06cb
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-04-12 19:50:59 -0500

    keydown and keyup

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 // Your code goes here
 
 
-//mouseover
+//1. mouseover
 let mouseOver = document.querySelector("img");
 mouseOver.addEventListener("mouseover", (event) => {
     event.target.style.transform = "scale(1.2)"
 } )
 
-//mouseleave
+//2. mouseleave
 let mouseLeave = document.querySelector("img");
 mouseLeave.addEventListener("mouseleave", (event) => {
     event.target.style.transform = "scale(1.0)"
 })
 
-//click
+//3. click
 let button = document.querySelectorAll('.btn');
 for (let b = 0; b < button.length; b++) {
     let btn = button[b];
@@ -22,3 +22,18 @@ for (let b = 0; b < button.length; b++) {
      event.stopPropagation();
     })
 };
+
+// 4. keydown -- down button
+document.addEventListener('keydown', (event) => {
+    if(event.keyCode == 40) //down button
+    document.body.style.background = "#E9DBB5";
+    event.stopPropagation();
+});
+
+// 5.  keyup -- up button
+document.addEventListener('keyup', (event) => {
+    if(event.keyCode == 38) // up button
+    document.body.style.background = "#d9d9d9";
+    event.stopPropagation();
+
+});
